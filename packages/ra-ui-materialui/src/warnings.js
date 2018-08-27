bf2c459b31089706a4f2e3e733c8b32d03065bc5
Kmaschta <maschtaler.kevin.iris@gmail.com>
2018-08-27 21:58:03 +0200

    Warn About Outdated Material-UI version

---
+++ packages/ra-ui-materialui/src/warnings.js
@@ -0,0 +1,16 @@
+import warning from 'warning';
+import { ToggleButtons } from '@material-ui/core';
+
+// ToogleButtons was introduced on Material-UI 1.4.0
+// which is the minimum supported version of React Admin
+// https://github.com/mui-org/material-ui/releases/tag/v1.4.0
+const muiVersionIsSupported = !!ToggleButtons;
+
+export const warnForUnsupportedMuiVersion = () => {
+    warning(
+        process.env.NODE_ENV === 'production' || muiVersionIsSupported,
+        `Your version of @material-ui/core is not supported by ra-ui-materialui!
+        Please install @material-ui/core@^1.4.0 in order to avoid uncertain UI behaviors on production.
+        More infos at https://github.com/marmelab/react-admin/issues/1782`
+    );
+};
